61421225bbcf42affb643c9b15a915137b7f0c82
PoryadochkovaKaterina <kd.poryadochkova@gmail.com>
2024-01-17 00:55:43 +0300

    Task

--- Task_1-3.py
+++ Task_1-3.py
@@ -5,45 +5,61 @@ if y == 0:
 p1, p2, p3, p4, p5 = x+y, x-y, x*y, x/y, x//y
 print(p1, p2, p3, p4, p5)   # для поверки значений
 print('Второе наибольшее из чисел: ')
-max_1 = p1
-max_2 = 0
+max_1, max_2 = 0, 0
 
-if p2 >= p1: max_1 = p2
-if p3 >= p1: max_1 = p3
-if p4 >= p1: max_1 = p4
-if p5 >= p1: max_1 = p5
+if p1 >= p2 and p1 >= p3 and p1 >= p4 and p1 >= p5: max_1 = p1
+if p2 >= p1 and p2 >= p3 and p2 >= p4 and p2 >= p5: max_1 = p2
+if p3 >= p1 and p3 >= p2 and p3 >= p4 and p3 >= p5: max_1 = p3
+if p4 >= p1 and p4 >= p2 and p4 >= p3 and p4 >= p5: max_1 = p4
+if p5 >= p1 and p5 >= p2 and p5 >= p3 and p5 >= p4: max_1 = p5
 
-if max_1 == p1 or max_1 == p2:
-    if p2 >= p3 and p2 >= p4 and p2 >= p5:
+if max_1 == p1:
+    if p2 > p3 and p2 > p4 and p2 > p5:
         max_2 = p2
+    else:
+        if p3 > p4 and p3 > p5:
+            max_2 = p3
+        else:
+            if p4 > p5:
+                max_2 = p4
+            else: max_2 = p5
+if max_1 == p2:
     if p1 >= p3 and p1 >= p4 and p1 >= p5:
         max_2 = p1
+    else:
         if p3 >= p4 and p3 >= p5:
             max_2 = p3
+        else:
             if p4 >= p5:
                 max_2 = p4
             else: max_2 = p5
 if max_1 == p3:
     if p1 >= p2 and p1 >= p4 and p1 >= p5:
         max_2 = p1
+    else:
         if p2 >= p4 and p2 >= p5:
             max_2 = p2
+        else:
             if p4 >= p5:
                 max_2 = p4
             else: max_2= p5
 if max_1 == p4:
     if p1 >= p2 and p1 >= p3 and p1 >= p5:
         max_2 = p1
+    else:
         if p2 >= p3 and p2 >= p5:
             max_2 = p2
+        else:
             if p3 >= p5:
                 max_2 = p3
             else: max_2 = p5
 if max_1 == p5:
     if p1 >= p2 and p1 >= p3 and p1 >= p4:
         max_2 = p1
+    else:
         if p2 >= p3 and p2 >= p4:
             max_2 = p2
+        else:
             if p3 >= p4:
                 max_2 = p3
             else: max_2 = p4
