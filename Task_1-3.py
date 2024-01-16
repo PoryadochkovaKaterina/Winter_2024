3f68889cc3f78f4d777b96b152325d21cbd0d805
PoryadochkovaKaterina <kd.poryadochkova@gmail.com>
2024-01-16 21:28:40 +0300

    Task 1_3

--- Task_1-3.py
+++ Task_1-3.py
@@ -3,15 +3,64 @@ if y == 0:
     print('Введите новое число, на 0 делить нельзя: ')
     y = int(input('Введите 2-е число: '))
 p1, p2, p3, p4, p5 = x+y, x-y, x*y, x/y, x//y
-k1, k2, k3, k4, k5 = 0, 0, 0, 0, 0
-print(p1, p2, p3, p4, p5)   # для проверки значений
+print(p1, p2, p3, p4, p5)   # для поверки значений
+print('Второе наибольшее из чисел:')
 if p1 >= p2 and p1 >= p3 and p1 >= p4 and p1 >= p5:
-    print('Сумма: ', p1)
-if p2 >= p1 and p2 >= p3 and p2 >= p4 and p2 >= p5:
-    print('Вычитание: ', p2)
-if p3 >= p1 and p3 >= p2 and p3 >= p4 and p3 >= p5:
-    print('Умножение: ', p3)
-if p4 >= p1 and p4 >= p2 and p4 >= p3 and p4 >= p5:
-    print('Деление: ', p4)
-if p5 >= p1 and p5 >= p2 and p5 >= p3 and p5 >= p4:
-    print('Целочисленное деление: ', p5)
+    if p2 >= p3 and p2 >= p4 and p2 >= p5:
+        print(p2)
+    else:
+        if p3 >= p4 and p3 >= p5:
+            print(p3)
+        else:
+            if p4 >= p5:
+                print(p4)
+            else:
+                print(p5)
+else:
+    if p2 >= p1 and p2 >= p3 and p2 >= p4 and p2 >= p5:
+        if p3 >= p1 and p3 >= p4 and p3 >= p5:
+            print(p3)
+        else:
+            if p4 >= p1 and p4 >= p5:
+                print(p4)
+            else:
+                if p1 >= p5:
+                    print(p1)
+                else:
+                    print(p5)
+    else:
+        if p3 >= p1 and p3 >= p2 and p3 >= p4 and p3 >= p5:
+            if p2 >= p1 and p2 >= p4 and p2 >= p5:
+                print(p2)
+            else:
+                if p4 >= p1 and p4 >= p5:
+                    print(p4)
+                else:
+                    if p1 >= p5:
+                        print(p1)
+                    else:
+                        print(p5)
+        else:
+            if p4 >= p1 and p4 >= p2 and p4 >= p3 and p4 >= p5:
+                if p1 >= p2 and p1 >= p3 and p1 >= p5:
+                    print(p1)
+                else:
+                    if p2 >= p3 and p2 >= p5:
+                        print(p2)
+                    else:
+                        if p3 >= p5:
+                            print(p3)
+                        else:
+                            print(p5)
+            else:
+                if p5 >= p1 and p5 >= p2 and p5 >= p3 and p5 >= p4:
+                    if p1 >= p2 and p1 >= p3 and p1 >= p4:
+                        print(p1)
+                    else:
+                        if p2 >= p3 and p2 >= p4:
+                            print(p2)
+                        else:
+                            if p3 >= p4:
+                                print(p3)
+                            else:
+                                print(p4)
