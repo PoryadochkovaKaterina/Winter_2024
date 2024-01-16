35d15d48d53f7f3198babd4ce0df14add702e54f
PoryadochkovaKaterina <kd.poryadochkova@gmail.com>
2024-01-17 00:28:13 +0300

    Task

--- Task_1-3.py
+++ Task_1-3.py
@@ -1,66 +1,51 @@
-x, y = int(input('Введите 1-ое число: ')), int(input('Введите 2-е число: '))
+x, y = float(input('Введите 1-ое число: ')), float(input('Введите 2-е число: '))
 if y == 0:
     print('Введите новое число, на 0 делить нельзя: ')
-    y = int(input('Введите 2-е число: '))
+    y = float(input('Введите 2-е число: '))
 p1, p2, p3, p4, p5 = x+y, x-y, x*y, x/y, x//y
 print(p1, p2, p3, p4, p5)   # для поверки значений
-print('Второе наибольшее из чисел:')
-if p1 >= p2 and p1 >= p3 and p1 >= p4 and p1 >= p5:
+print('Второе наибольшее из чисел: ')
+max_1 = p1
+max_2 = 0
+
+if p2 >= p1: max_1 = p2
+if p3 >= p1: max_1 = p3
+if p4 >= p1: max_1 = p4
+if p5 >= p1: max_1 = p5
+
+if max_1 == p1 or max_1 == p2:
     if p2 >= p3 and p2 >= p4 and p2 >= p5:
-        print(p2)
-    else:
+        max_2 = p2
+    if p1 >= p3 and p1 >= p4 and p1 >= p5:
+        max_2 = p1
         if p3 >= p4 and p3 >= p5:
-            print(p3)
-        else:
+            max_2 = p3
             if p4 >= p5:
-                print(p4)
-            else:
-                print(p5)
-else:
-    if p2 >= p1 and p2 >= p3 and p2 >= p4 and p2 >= p5:
-        if p3 >= p1 and p3 >= p4 and p3 >= p5:
-            print(p3)
-        else:
-            if p4 >= p1 and p4 >= p5:
-                print(p4)
-            else:
-                if p1 >= p5:
-                    print(p1)
-                else:
-                    print(p5)
-    else:
-        if p3 >= p1 and p3 >= p2 and p3 >= p4 and p3 >= p5:
-            if p2 >= p1 and p2 >= p4 and p2 >= p5:
-                print(p2)
-            else:
-                if p4 >= p1 and p4 >= p5:
-                    print(p4)
-                else:
-                    if p1 >= p5:
-                        print(p1)
-                    else:
-                        print(p5)
-        else:
-            if p4 >= p1 and p4 >= p2 and p4 >= p3 and p4 >= p5:
-                if p1 >= p2 and p1 >= p3 and p1 >= p5:
-                    print(p1)
-                else:
-                    if p2 >= p3 and p2 >= p5:
-                        print(p2)
-                    else:
-                        if p3 >= p5:
-                            print(p3)
-                        else:
-                            print(p5)
-            else:
-                if p5 >= p1 and p5 >= p2 and p5 >= p3 and p5 >= p4:
-                    if p1 >= p2 and p1 >= p3 and p1 >= p4:
-                        print(p1)
-                    else:
-                        if p2 >= p3 and p2 >= p4:
-                            print(p2)
-                        else:
-                            if p3 >= p4:
-                                print(p3)
-                            else:
-                                print(p4)
+                max_2 = p4
+            else: max_2 = p5
+if max_1 == p3:
+    if p1 >= p2 and p1 >= p4 and p1 >= p5:
+        max_2 = p1
+        if p2 >= p4 and p2 >= p5:
+            max_2 = p2
+            if p4 >= p5:
+                max_2 = p4
+            else: max_2= p5
+if max_1 == p4:
+    if p1 >= p2 and p1 >= p3 and p1 >= p5:
+        max_2 = p1
+        if p2 >= p3 and p2 >= p5:
+            max_2 = p2
+            if p3 >= p5:
+                max_2 = p3
+            else: max_2 = p5
+if max_1 == p5:
+    if p1 >= p2 and p1 >= p3 and p1 >= p4:
+        max_2 = p1
+        if p2 >= p3 and p2 >= p4:
+            max_2 = p2
+            if p3 >= p4:
+                max_2 = p3
+            else: max_2 = p4
+
+print(max_2)
