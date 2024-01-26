049feecf145c58dcf96aab790a170ccf3f5c5f0d
PoryadochkovaKaterina <kd.poryadochkova@gmail.com>
2024-01-26 18:35:40 +0300

    Task 5

--- Task_5/Task_5-2.py
+++ Task_5/Task_5-2.py
@@ -1,9 +1,19 @@
 n = int(input())
 f = []
+r = []
+x = 0
 for i in range(1, n+1):
     if n % i == 0:
         f.append(i)
-        # print(f)
     else: continue
+for k in f[1:]:
+    for t in range(2, k):
+        if k % t == 0:
+            x = x + 1
+    if x == 0:
+        r.append(k)
+    else:
+       x = 0
+        # print(f)
 print(' '.join(map(str, f)))
-
+print(' '.join(map(str, r)))
--- Task_5/Task_5-3.py
+++ Task_5/Task_5-3.py
@@ -1,9 +1,19 @@
 n = int(input())
 res = []
 r = [1, 1]
-for i in range(n):
-    v = r[i] + r[i+1]
+f = [1, 1]
+f1, f2 = 1, 1
+k = 2
+for i in range(n + 1):
+    v = r[i] + r[i + 1]
     if v <= n:
         r.append(v)
-    else: break
+    else:
+        break
 print(' '.join(map(str, r)))
+
+while k < n:
+    f1, f2 = f2, f1 + f2
+    f.append(f2)
+    k += 1
+print(' '.join(map(str, f)))
