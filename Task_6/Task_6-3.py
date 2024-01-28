1c7bec772f5249bef1a8a14797f7c8beb52cc574
PoryadochkovaKaterina <kd.poryadochkova@gmail.com>
2024-01-28 12:32:03 +0300

    Task

---
+++ Task_6/Task_6-3.py
@@ -0,0 +1,13 @@
+import string
+n = input()
+w, p, d = [], [], []
+for i in n:
+    if i in string.ascii_letters:
+        w.append(i)
+    if i in string.punctuation:
+        p.append(i)
+    if i in string.digits:
+        d.append(i)
+print(' '.join(set(w)))
+print(' '.join(set(p)))
+print(' '.join(set(d)))
