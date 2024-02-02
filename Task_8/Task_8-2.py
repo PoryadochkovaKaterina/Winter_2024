2f297e3ea7baeb7ee88ef40d6aa95d43e9195039
PoryadochkovaKaterina <kd.poryadochkova@gmail.com>
2024-02-02 18:27:16 +0300

    Task

--- Task_8/Task_8-2.py
+++ Task_8/Task_8-2.py
@@ -1,11 +1,22 @@
-d = [[1, 2, 3], [2, 44, 5, 4], [303, 456]]
-l = []
+d = [[1, 458, 27, 3], [333333333333], [2, 44, 5, 4], [3, 476, 56], [0], [2, 1, 3]]
+l, v = [], []
+res = []
 for i in d:
     pr = ''.join(map(str, i))
     l.append(pr)
     l.sort(key=lambda pt: len(''.join(map(str, pt))))
-print(l)
 
+for k in l:
+    for j in d:
+        fr = ''.join(map(str, j))
+        if k == fr:
+            v.append(j)
+        else:
+            continue
 
+for t in v:
+    k = sorted(t)
+    k.reverse()
+    res.append(k)
 
-
+print(d, '\n', res)
