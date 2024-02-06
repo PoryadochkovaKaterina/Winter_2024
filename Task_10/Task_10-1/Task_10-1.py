bee1856aee61295f49418645ea11b080bb36a5eb
PoryadochkovaKaterina <kd.poryadochkova@gmail.com>
2024-02-06 23:40:08 +0300

    Task

---
+++ Task_10/Task_10-1/Task_10-1.py
@@ -0,0 +1,20 @@
+f = open('test1.txt', encoding='utf-8')
+s = f.readlines()
+
+f_out = open('text2.txt', 'w', encoding='utf-8')
+r = []
+for i in s:
+    r.append(i.split())
+    #print(r)
+
+for j in r:
+    j.reverse()
+    result = " ".join(j)
+    #print(result)
+    f_out.writelines(result + '\n')
+
+f_out = open('text2.txt', 'r', encoding='utf-8')
+s_out = f_out.read()
+print(s_out)
+f_out.close()
+f.close()
