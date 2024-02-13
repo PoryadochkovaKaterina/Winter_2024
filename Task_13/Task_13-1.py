932ae47c8675cbe853001b093ef48544424d93ca
PoryadochkovaKaterina <kd.poryadochkova@gmail.com>
2024-02-13 21:06:58 +0300

    Task

---
+++ Task_13/Task_13-1.py
@@ -0,0 +1,16 @@
+def pos(n):
+    cou = 0
+    while True:
+        for i in range(1, n + 1):
+            if cou < n:
+                if i % 2:
+                    yield i
+                    cou += 1
+                else:
+                    yield -i
+                    cou += 1
+            else: break
+
+df = pos(int(input()))
+for k in df:
+    print(k, end=' ')
