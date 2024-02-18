db8ce8fde59df077fac0faff6111c91f592c715b
PoryadochkovaKaterina <kd.poryadochkova@gmail.com>
2024-02-18 15:10:59 +0300

    Task

--- Task_15/Task_15-2.py
+++ Task_15/Task_15-2.py
@@ -4,9 +4,8 @@ import re
 nom = input().upper()
 # print(nom)
 
-reg_ru = r'[АВКЕМНОРСТУХ]\d{3}[АВКЕМНОРСТУХ]{2}78|[АВКЕМНОРСТУХ]\d{3}[АВКЕМНОРСТУХ]{2}178'
-reg_en = r'[ABEKMHOPCTYXD]\d{3}[ABEKMHOPCTYXD]{2}78|[ABEKMHOPCTYXD]\d{3}[ABEKMHOPCTYXD]{2}178'
-num_ru = re.findall(reg_ru, nom)
-num_en = re.findall(reg_en, nom)
+reg = r'[АВКЕМНОРСТУХABEKMHOPCTYXD]\d{3}[АВКЕМНОРСТУХABEKMHOPCTYXD]{2}(?:78|178)\b'
 
-print(*num_ru, *num_en)
+num = re.findall(reg, nom)
+
+print(*num)
