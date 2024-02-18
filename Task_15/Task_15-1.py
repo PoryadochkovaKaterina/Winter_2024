1558c3332b7c6bb9589940ad4370c5f054d34ce9
PoryadochkovaKaterina <kd.poryadochkova@gmail.com>
2024-02-18 18:17:58 +0300

    Task

--- Task_15/Task_15-1.py
+++ Task_15/Task_15-1.py
@@ -1,4 +1,5 @@
-dic =   {1: 1,
+dic =   {1: {1: 11111,
+             2: 22222,},
         2: 2,
         3: {4: 11,
            5: 33,
@@ -18,9 +19,10 @@ def rek(dic):
     for i, v in dic.items():
         # print(i, v)
         if type(v) is dict:
+            yield (i, v)
             yield from rek(v)
         else:
-            yield (i,v)
+            yield (i, v)
 
 for i, v in rek(dic):
     if i == 1:
