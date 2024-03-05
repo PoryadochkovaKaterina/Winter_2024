7b69d5738e5a332ec92e52c423c516b5ef99765c
PoryadochkovaKaterina <kd.poryadochkova@gmail.com>
2024-03-05 20:28:58 +0300

    Task

--- Task_20/Task_20-2.py
+++ Task_20/Task_20-2.py
@@ -1,13 +1,24 @@
 import pandas as pd
+import re
 def sum_Data(lst):
     df = pd.DataFrame(lst)
     res = []
     for i in df.index:
         for j in df.columns:
-            # print(type(df.loc[i, j]), df.loc[i, j])
-            # if (not df.loc[i, j].isalpha()) or (df.loc[i, j].isdigit()):
-            if df.loc[i, j].isdigit():
+            # res_n = isinstance(df.loc[i, j], int)
+            if re.findall(r'(\b\d+\b)|(\b\d+.\d+\b)|(\b\d+,\d+\b)', df.loc[i, j]):
+                # print(type(df.loc[i, j]))
                 res.append(float(df.loc[i, j]))
     return sum(res)
 print(sum_Data(input().split()))
 
+
+# s = 0
+# for i in df.index:
+#     for j in df.columns:
+#         #if type(df.loc[i, j]) == np.int64 or type(df.loc[i, j]) == np.floot64:
+#         try:
+#             s += df.loc[i, j]
+#         except:
+#             pass
+# print(s)
