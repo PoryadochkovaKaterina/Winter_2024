11b2609f30b0efb6efae1a8e463b70d1e1375644
PoryadochkovaKaterina <kd.poryadochkova@gmail.com>
2024-03-14 20:14:06 +0300

    Task

--- Task_24/Task_24-1.py
+++ Task_24/Task_24-1.py
@@ -1,14 +1,14 @@
-# # Максимальное значение
-# lis = [76, 34, 54, 9, 79]
-# max_ = lis[0]
-# for ele in lis:
-#     if ele > max_:   # < для минимального значения
-#         max_ = ele
-# print(max_)
+# Максимальное значение
+lis = [76, 34, 54, 9, 79]
+maxi = lis[0]
+for num in lis:
+    if num > maxi:   # < для минимального значения
+        maxi = num
+print(maxi)
 
 
 # В порядке возрастания
-lis_1 = [76, 23, 45, 12, 54, 9]
+lis_1 = [76, 23, 45, 12, 54, 9, 34]
 print(*lis_1)
 for i in range(0, len(lis_1)):
     for j in range(i + 1, len(lis_1)):
