1750516e412036cc7806effdf0c7a261aa7e9758
PoryadochkovaKaterina <kd.poryadochkova@gmail.com>
2024-03-17 14:56:55 +0300

    Task

--- Task_25/Task_25-2.py
+++ Task_25/Task_25-2.py
@@ -1,1 +1,9 @@
+def is_pal(string):
+    if len(string) <= 1:
+        return 'True'
+    if string[0] != string[-1]:
+        return 'False'
+    return is_pal(string[1:-1])
+
+print(is_pal(''.join(input().lower().split())))
 
