f601f7f9360f92b8c0cc7c02e2154e0ab05f8ae6
PoryadochkovaKaterina <kd.poryadochkova@gmail.com>
2024-03-25 18:57:39 +0300

    Lesson

--- Task_28/Task_28-1.py
+++ Task_28/Task_28-1.py
@@ -7,8 +7,12 @@ def inversion(spisok, cou=0):
                 cou += 1
     return cou
 
-# print(inversion([5, 4, 3, 2, 1]))
-# print(inversion([1, 2, 3, 4, 5]))
-# print(inversion([1, 4, 3, 0, 5]))
-# print(inversion([10, 5, 3, 6, 7]))
+print(inversion([5, 4, 3, 2, 1]))
+print(inversion([1, 2, 3, 4, 5]))
+print(inversion([1, 4, 3, 0, 5]))
+print(inversion([10, 5, 3, 6, 7]))
 print(inversion([0, 3, 4, 2, 0, 2]))
+
+# lst = [5, 4, 3, 2, 1]
+# lent = len(lst)
+# print(sum([lst[i] > lst[j] for i in range(lent - 1) for j in range(i + 1, lent)]))
